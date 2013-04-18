3c8a6252704a9ab8dd478e73281b273483c73eae
Gianluca Della Vedova <gianluca@dellavedova.org>
2013-04-18 09:50:09 +0200

    soluzione Gen 2013

---
+++ esami/2013/2013-01.sql
@@ -0,0 +1,4 @@
+    Determinare il seggio dove si sono verificati il numero massimo di voti validi.
+    Determinare i candidati che sono presenti in più circoscrizioni.
+    Scrivere la condizione che verifica che il vincolo “non è possibile dare una preferenza senza indicare una lista” sia soddisfatto.
+    Determinare i candidati che non hanno ricevuto preferenze.
