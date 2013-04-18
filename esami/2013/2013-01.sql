1a3e5f33d23798c9ad991dea3b96e98f22693298
Gianluca Della Vedova <gianluca@dellavedova.org>
2013-04-18 10:35:30 +0200

    codice sql Gen 2013

--- esami/2013/2013-01.sql
+++ esami/2013/2013-01.sql
@@ -1,4 +1,29 @@
-    Determinare il seggio dove si sono verificati il numero massimo di voti validi.
-    Determinare i candidati che sono presenti in più circoscrizioni.
-    Scrivere la condizione che verifica che il vincolo “non è possibile dare una preferenza senza indicare una lista” sia soddisfatto.
-    Determinare i candidati che non hanno ricevuto preferenze.
+-- Determinare il seggio dove si sono verificati il numero massimo di voti validi.
+
+create view voti_per_seggio (seggio, voto) as
+       select seggio, count(*)
+       from voto
+       group by seggio
+
+select *
+from voti_per_seggio
+where voti >= all (select max(voti)
+      	      	   from voti_per_seggio)
+
+
+-- Scrivere la condizione che verifica che il vincolo “non è possibile dare una preferenza senza indicare una lista” sia soddisfatto.
+
+create table voto (
+ id integer primary key,
+ ....
+ preferenza char(16) references candidato(codicefiscale) null
+)
+
+
+-- Determinare i candidati che non hanno ricevuto preferenze.
+
+select codicefiscale
+from candidato
+where not exists (select *
+      	  	  from voto
+		  where preferenza.codicefiscale=candidato.codicefiscale)
